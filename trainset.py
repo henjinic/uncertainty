a989dbdbf6724bf2d23b2c31d4d754676ac69ee0
Henjinic <henjinic@gmail.com>
2019-09-20 17:40:20 +0900

    trainset.py: upward option and id-ing option

--- trainset.py
+++ trainset.py
@@ -12,10 +12,14 @@ import common
 import numpy as np
 
 FULLSET_PATH = f'{common.BASE_DIR}/fullset.csv'
-OUTPUT_PATH = f'{common.BASE_DIR}/trainset.csv'
+OUTPUT_PATH = f'{common.BASE_DIR}/trainset_clust_up.csv'
 
-def get_trainset(fullset):
+def get_trainset(fullset, upward=False, with_id=False):
     result = []
+
+    if upward:
+        fullset = np.flipud(fullset.reshape(common.N_ROWS, common.N_COLS, -1)).reshape(-1, fullset.shape[-1])
+
     for row in fullset:
         disaster_prob_vec = row[:common.N_DISASTER]
         feature_vec = row[common.N_DISASTER:]
@@ -26,14 +30,17 @@ def get_trainset(fullset):
             label = common.classify(categorized_vec)
             result.append(feature_vec.tolist() + [label])
 
-    return np.array(result)
+    if with_id:
+        return np.concatenate((np.arange(len(result))[:, np.newaxis], result), axis=1)
+    else:
+        return np.array(result)
 
 def main():
     print('Loading...')
     fullset = common.load_data(FULLSET_PATH, sep=',')
 
     print('Processing...')
-    trainset = get_trainset(fullset)
+    trainset = get_trainset(fullset, upward=True)
 
     print('Saving...')
     common.save_data(trainset, OUTPUT_PATH)
